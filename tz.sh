e551ad7e00ee277999cb5580b12f294d8436d6e8
LIU JIAN <jianliu@thoughtworks.com>
2020-05-29 13:15:48 +1200

    Create tz.sh

---
+++ tz.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt update
+# set noninteractive installation
+export DEBIAN_FRONTEND=noninteractive
+#install tzdata package
+apt-get install -y tzdata
+# set your timezone
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
